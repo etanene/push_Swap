86907d201f445a8d19c8647b814ae96dbfd16f4b
Arnbjorn Falmer archer <afalmer-@mi-b5.21-school.ru>
2019-04-13 19:55:35 +0300

    correct set stack

--- checkerdir/main.c
+++ checkerdir/main.c
@@ -6,7 +6,7 @@
 /*   By: afalmer- <afalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 19:19:19 by afalmer-          #+#    #+#             */
-/*   Updated: 2019/04/13 19:30:20 by afalmer-         ###   ########.fr       */
+/*   Updated: 2019/04/13 19:53:56 by afalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,79 +79,95 @@ t_stack	*ft_set_stack(char **av, int ac)
 	char	**nums;
 
 	stack = NULL;
-	nums = ft_strsplit(*av, ' ');
-	count = ft_arrlen(nums);
-	while (count--)
+	if (ac == 1)
 	{
-		if (!ft_add_num(nums[count], &stack))
-			return (NULL);
+		nums = ft_strsplit(*av, ' ');
+		count = ft_arrlen(nums);
+		while (count--)
+		{
+			if (!ft_add_num(nums[count], &stack))
+				return (NULL);
+		}
 	}
-	while (ac--)
+	else
 	{
-		if (!ft_add_num(av[ac], &stack))
-			return (NULL);
+		while (ac--)
+		{
+			if (!ft_add_num(av[ac], &stack))
+				return (NULL);
+		}
 	}
 	return (stack);
+}
+
+int		ft_read_input(t_stack **a, t_stack **b)
+{
+	char	*op;
+
+	while (get_next_line(0, &op) > 0)
+	{
+		if (!ft_strcmp(op, "pa"))
+			ft_push_op(b, a);
+		else if (!ft_strcmp(op, "pb"))
+			ft_push_op(b, a);
+		else if (!ft_strcmp(op, "sa"))
+			ft_swap_op(a);
+		else if (!ft_strcmp(op, "sb"))
+			ft_swap_op(b);
+		else if (!ft_strcmp(op, "ss"))
+		{
+			ft_swap_op(a);
+			ft_swap_op(b);
+		}
+		else if (!ft_strcmp(op, "ra"))
+			ft_rotate_op(a);
+		else if (!ft_strcmp(op, "rb"))
+			ft_rotate_op(b);
+		else if (!ft_strcmp(op, "rr"))
+		{
+			ft_rotate_op(a);
+			ft_rotate_op(b);
+		}
+		else if (!ft_strcmp(op, "rra"))
+			ft_revrotate_op(a);
+		else if (!ft_strcmp(op, "rrb"))
+			ft_revrotate_op(b);
+		else if (!ft_strcmp(op, "rrr"))
+		{
+			ft_revrotate_op(a);
+			ft_revrotate_op(b);
+		}
+		else
+		{
+			ft_strdel(&op);
+			return (0);
+		}
+		ft_strdel(&op);
+		ft_print_stack(*a, 'a');
+		ft_print_stack(*b, 'b');
+	}
+	return (1);
 }
 
 int		main(int ac, char **av)
 {
 	t_stack	*a;
 	t_stack	*b;
-	char	*op;
 
 	b = NULL;
 	if (ac == 1)
 		return (0);
-	if ((a = ft_set_stack(++av, ac - 2)) == NULL)
+	if ((a = ft_set_stack(++av, ac - 1)) == NULL)
 	{
 		ft_putstr("Error\n");
 		return (0);
 	}
 	ft_print_stack(a, 'a');
 	ft_print_stack(b, 'b');
-	while (get_next_line(0, &op) > 0)
+	if (!ft_read_input(&a, &b))
 	{
-		if (!ft_strcmp(op, "pa"))
-			ft_push_op(&b, &a);
-		else if (!ft_strcmp(op, "pb"))
-			ft_push_op(&a, &b);
-		else if (!ft_strcmp(op, "sa"))
-			ft_swap_op(&a);
-		else if (!ft_strcmp(op, "sb"))
-			ft_swap_op(&b);
-		else if (!ft_strcmp(op, "ss"))
-		{
-			ft_swap_op(&a);
-			ft_swap_op(&b);
-		}
-		else if (!ft_strcmp(op, "ra"))
-			ft_rotate_op(&a);
-		else if (!ft_strcmp(op, "rb"))
-			ft_rotate_op(&b);
-		else if (!ft_strcmp(op, "rr"))
-		{
-			ft_rotate_op(&a);
-			ft_rotate_op(&b);
-		}
-		else if (!ft_strcmp(op, "rra"))
-			ft_revrotate_op(&a);
-		else if (!ft_strcmp(op, "rrb"))
-			ft_revrotate_op(&b);
-		else if (!ft_strcmp(op, "rrr"))
-		{
-			ft_revrotate_op(&a);
-			ft_revrotate_op(&b);
-		}
-		else
-		{
-			ft_strdel(&op);
-			ft_putstr("Error\n");
-			return (0);
-		}
-		ft_strdel(&op);
-		ft_print_stack(a, 'a');
-		ft_print_stack(b, 'b');
+		ft_putstr("Error\n");
+		return (0);
 	}
 	if (ft_is_sort(a))
 		printf("OK\n");

f3192d705a7e7fc576b067023f4377decddd30d3
Arnbjorn Falmer archer <afalmer-@mi-b5.21-school.ru>
2019-04-19 20:27:06 +0300

    test sort 3 numbers

--- push_swap.c
+++ push_swap.c
@@ -6,152 +6,13 @@
 /*   By: afalmer- <afalmer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 20:02:29 by afalmer-          #+#    #+#             */
-/*   Updated: 2019/04/18 18:50:32 by afalmer-         ###   ########.fr       */
+/*   Updated: 2019/04/19 19:17:32 by afalmer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // ruby -e "puts (-20 .. 20).to_a.shuffle.join(' ')"
 
 #include "push_swap.h"
-
-// void	ft_sort(t_stack **main, t_stack **off, int pivot, int count);
-// void	ft_revsort(t_stack **main, t_stack **off, int pivot, int count);
-
-// void	ft_revsort(t_stack **main, t_stack **off, int pivot, int count)
-// {
-// 	int		mainsize;
-// 	int		offsize;
-// 	int		i;
-
-// 	mainsize = 0;
-// 	offsize = 0;
-// 	ft_printf("REV_START\n");
-// 	ft_printf("pivot: %d\n", pivot);
-// 	ft_printf("count: %d\n", count);
-// 	ft_print_ab(*main, *off);
-// 	if (count < 2)
-// 	{
-// 		if (count == 1)
-// 		{
-// 			ft_push_op(main, off);
-// 			ft_printf("pa\n");
-// 			ft_print_ab(*main, *off);
-// 		}
-// 		return ;
-// 	}
-// 	i = 0;
-// 	while (i++ < count)
-// 	{
-// 		if ((*main)->value > pivot)
-// 		{
-// 			ft_push_op(main, off);
-// 			ft_printf("pa\n");
-// 			ft_print_ab(*main, *off);
-// 			offsize++;
-// 		}
-// 		else
-// 		{
-// 			ft_rotate_op(main);
-// 			ft_printf("rb\n");
-// 			ft_print_ab(*main, *off);
-// 			mainsize++;
-// 		}
-// 	}
-// 	ft_printf("main: %d, off: %d\n", mainsize, offsize);
-// 	i = 0;
-// 	// if (mainsize < offsize && (*main)->value != pivot)
-// 	// {
-// 		while (i++ < mainsize)
-// 		{
-// 			ft_revrotate_op(main);
-// 			ft_printf("rrb\n");
-// 			ft_print_ab(*main, *off);
-// 		}
-// 	// }
-// 	// else if ((*main)->value != pivot)
-// 	// {
-// 	// 	while (i++ < offsize)
-// 	// 	{
-// 	// 		ft_rotate_op(main);
-// 	// 		ft_printf("rb\n");
-// 	// 		ft_print_ab(*main, *off);
-// 	// 	}
-// 	// }
-// 	if (pivot == (*main)->value && mainsize != 1)
-// 	{
-// 		ft_swap_op(main);
-// 		ft_printf("sb\n");
-// 		ft_print_ab(*main, *off);
-// 	}
-// 	ft_sort(off, main, (*off)->value, offsize);
-// 	ft_revsort(main, off, (*main)->value, mainsize);
-// }
-
-// void	ft_sort(t_stack **main, t_stack **off, int pivot, int count)
-// {
-// 	int		mainsize;
-// 	int		offsize;
-// 	int		i;
-
-// 	mainsize = 0;
-// 	offsize = 0;
-// 	ft_printf("START\n");
-// 	ft_printf("pivot: %d\n", pivot);
-// 	ft_printf("count: %d\n", count);
-// 	ft_print_ab(*main, *off);
-// 	if (count < 2)
-// 	{
-// 		return ;
-// 	}
-// 	i = 0;
-// 	while (i++ < count)
-// 	{
-// 		if ((*main)->value < pivot)
-// 		{
-// 			ft_push_op(main, off);
-// 			ft_printf("pb\n");
-// 			ft_print_ab(*main, *off);
-// 			offsize++;
-// 		}
-// 		else
-// 		{
-// 			ft_rotate_op(main);
-// 			ft_printf("ra\n");
-// 			ft_print_ab(*main, *off);
-// 			mainsize++;
-// 		}
-// 	}
-// 	ft_printf("main: %d, off: %d\n", mainsize, offsize);
-// 	i = 0;
-// 	// if (mainsize < offsize && (*main)->value != pivot)
-// 	// {
-// 		while (i++ < mainsize)
-// 		{
-// 			ft_revrotate_op(main);
-// 			ft_printf("rra\n");
-// 			ft_print_ab(*main, *off);
-// 		}
-// 	// }
-// 	// else if ((*main)->value != pivot)
-// 	// {
-// 	// 	while (i++ < offsize)
-// 	// 	{
-// 	// 		ft_rotate_op(main);
-// 	// 		ft_printf("ra\n");
-// 	// 		ft_print_ab(*main, *off);
-// 	// 	}
-// 	// }
-// 	if (pivot == (*main)->value && mainsize != 1)
-// 	{
-// 		ft_swap_op(main);
-// 		ft_printf("sa\n");
-// 		ft_print_ab(*main, *off);
-// 	}
-// 	if (*main)
-// 		ft_sort(main, off, (*main)->value, mainsize);
-// 	if (*off)
-// 		ft_revsort(off, main, (*off)->value, offsize);
-// }
 
 void	ft_sort(t_stack **a, t_stack **b, int size);
 void	ft_revsort(t_stack **b, t_stack **a, int size);
@@ -276,6 +137,26 @@ void	ft_sort(t_stack **a, t_stack **b, int size)
 	ft_revsort(b, a, bsize);
 }
 
+int		ft_is_sort_size(t_stack *stack, int size)
+{
+	while (stack->next && size--)
+	{
+		if (stack->value > stack->next->value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+void	ft_sort_size(t_stack **stack, int size)
+{
+	while (!ft_is_sort_size(*stack, size))
+	{
+		ft_swap_op(stack);
+		ft_printf("sa\n");
+	}
+}
+
 int		main(int ac, char **av)
 {
 	t_stack	*a;
@@ -290,7 +171,9 @@ int		main(int ac, char **av)
 		return (0);
 	}
 	ft_print_ab(a, b);
-	ft_sort(&a, &b, ac - 1);
+	ft_sort3(&a);
+	ft_print_ab(a, b);
+	// ft_sort(&a, &b, ac - 1);
 	// ft_printf("END\n");
 	// ft_print_ab(a, b);
 	return (0);
